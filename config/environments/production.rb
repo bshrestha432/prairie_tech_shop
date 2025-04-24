f00e6853501648722c5de6ba73b039067f546636
Bishal Shrestha <bshrestha@rrc.ca>
2025-04-24 15:58:43 -0500

    Last commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # Code is not reloaded between request.
   config.enable_reloading = false
 
   # Eager load code on boot. This eager loads most of Rails and
