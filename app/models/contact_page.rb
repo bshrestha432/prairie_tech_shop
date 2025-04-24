9fd9031d71a21f99b5ed5c4ce9120f0f6730ddc6
Bishal Shrestha <bshrestha@rrc.ca>
2025-04-24 15:39:53 -0500

    Fix tax rounding error on invoice

--- app/models/contact_page.rb
+++ app/models/contact_page.rb
@@ -1,6 +1,6 @@
 class ContactPage < ApplicationRecord
   has_rich_text :content
-  # Define attributes that are searchable with Ransack
+  # Define attribute that are searchable with Ransack
   def self.ransackable_attributes(auth_object = nil)
     ["title", "content"]
   end
